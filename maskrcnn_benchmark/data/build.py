c70d94134ee17b6615dc0d9dab23d2b6411619eb
KelvinCPChiu <40659980+KelvinCPChiu@users.noreply.github.com>
2019-06-05 15:43:22 +0800

    Add files via upload

--- maskrcnn_benchmark/data/build.py
+++ maskrcnn_benchmark/data/build.py
@@ -6,12 +6,11 @@ import logging
 import torch.utils.data
 from maskrcnn_benchmark.utils.comm import get_world_size
 from maskrcnn_benchmark.utils.imports import import_file
-from maskrcnn_benchmark.utils.miscellaneous import save_labels
 
 from . import datasets as D
 from . import samplers
 
-from .collate_batch import BatchCollator, BBoxAugCollator
+from .collate_batch import BatchCollator
 from .transforms import build_transforms
 
 
@@ -111,8 +110,8 @@ def make_data_loader(cfg, is_train=True, is_distributed=False, start_iter=0):
         images_per_batch = cfg.SOLVER.IMS_PER_BATCH
         assert (
             images_per_batch % num_gpus == 0
-        ), "SOLVER.IMS_PER_BATCH ({}) must be divisible by the number of GPUs ({}) used.".format(
-            images_per_batch, num_gpus)
+        ), "SOLVER.IMS_PER_BATCH ({}) must be divisible by the number "
+        "of GPUs ({}) used.".format(images_per_batch, num_gpus)
         images_per_gpu = images_per_batch // num_gpus
         shuffle = True
         num_iters = cfg.SOLVER.MAX_ITER
@@ -120,8 +119,8 @@ def make_data_loader(cfg, is_train=True, is_distributed=False, start_iter=0):
         images_per_batch = cfg.TEST.IMS_PER_BATCH
         assert (
             images_per_batch % num_gpus == 0
-        ), "TEST.IMS_PER_BATCH ({}) must be divisible by the number of GPUs ({}) used.".format(
-            images_per_batch, num_gpus)
+        ), "TEST.IMS_PER_BATCH ({}) must be divisible by the number "
+        "of GPUs ({}) used.".format(images_per_batch, num_gpus)
         images_per_gpu = images_per_batch // num_gpus
         shuffle = False if not is_distributed else True
         num_iters = None
@@ -151,13 +150,19 @@ def make_data_loader(cfg, is_train=True, is_distributed=False, start_iter=0):
     DatasetCatalog = paths_catalog.DatasetCatalog
     dataset_list = cfg.DATASETS.TRAIN if is_train else cfg.DATASETS.TEST
 
-    # If bbox aug is enabled in testing, simply set transforms to None and we will apply transforms later
-    transforms = None if not is_train and cfg.TEST.BBOX_AUG.ENABLED else build_transforms(cfg, is_train)
-    datasets = build_dataset(dataset_list, transforms, DatasetCatalog, is_train)
+#    transforms = []
+#    TODO : rewrite this so that it can take different transformer for different dataset
+    for dataset_name in dataset_list:
+        data = DatasetCatalog.get(dataset_name)
+        data_dir = data['args']['data_dir']
+        if 'tless' and 'train' in dataset_name:
+            print("####################################### Background Transform is loading. \
+            #######################################")
+            transforms = build_transforms(cfg, is_train, data_dir)
+        else:
+            transforms = build_transforms(cfg, is_train)
 
-    if is_train:
-        # save category_id to label name mapping
-        save_labels(datasets, cfg.OUTPUT_DIR)
+    datasets = build_dataset(dataset_list, transforms, DatasetCatalog, is_train)
 
     data_loaders = []
     for dataset in datasets:
@@ -165,8 +170,7 @@ def make_data_loader(cfg, is_train=True, is_distributed=False, start_iter=0):
         batch_sampler = make_batch_data_sampler(
             dataset, sampler, aspect_grouping, images_per_gpu, num_iters, start_iter
         )
-        collator = BBoxAugCollator() if not is_train and cfg.TEST.BBOX_AUG.ENABLED else \
-            BatchCollator(cfg.DATALOADER.SIZE_DIVISIBILITY)
+        collator = BatchCollator(cfg.DATALOADER.SIZE_DIVISIBILITY)
         num_workers = cfg.DATALOADER.NUM_WORKERS
         data_loader = torch.utils.data.DataLoader(
             dataset,
